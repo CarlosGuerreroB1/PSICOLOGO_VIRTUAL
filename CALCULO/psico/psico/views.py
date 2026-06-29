c58e181ce6058c7a7b6e31c56e81892eac230e54
CarlosGuerreroB1 <147994965+CarlosGuerreroB1@users.noreply.github.com>
2026-06-29 00:43:48 -0500

    Delete CALCULO directory

--- CALCULO/psico/psico/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-    return render(request, 'index/home.html')
-
-def chat(request):                                    # ← agrega esto
-    return render(request, 'index/chat.html')
